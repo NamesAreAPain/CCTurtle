2b591438f822a89ba57b26ec6d085e0e06dc2c6f
James Crocker <james@crocker.us>
2021-01-22 00:10:28 -0800

    t

--- shophost.lua
+++ shophost.lua
@@ -43,15 +43,15 @@ function Shop:drawPrices()
 end
 
 function Shop:drawCashOut()
-    self.monitor.setCursor(1,15)
+    self.monitor.setCursorPos(1,15)
     self.monitor.blit(centerText("REDEEM",18),pad(colors.toBlit(colors.orange),18),pad(colors.toBlit(colors.red),18))
-    self.monitor.setCursor(1,16)
+    self.monitor.setCursorPos(1,16)
     self.monitor.blit(centerText(({sanitize(self.name)})[1],18),pad(colors.toBlit(colors.orange),18),pad(colors.toBlit(colors.red),18))
     self.monitor.setCursor(1,17)
     self.monitor.blit(pad(" ",18),pad(colors.toBlit(colors.white),18),pad(colors.toBlit(colors.purple),18))
-    self.monitor.setCursor(1,18)
+    self.monitor.setCursorPos(1,18)
     self.monitor.blit(center("CLICK HERE",18),pad(colors.toBlit(colors.white),18),pad(colors.toBlit(colors.purple),18))
-    self.monitor.setCursor(1,19)
+    self.monitor.setCursorPos(1,19)
     self.monitor.blit(pad(" ",18),pad(colors.toBlit(colors.white),18),pad(colors.toBlit(colors.purple),18))
 end
 
